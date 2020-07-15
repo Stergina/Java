5ccd57dd6317dfe8c915738f961cd55e1407b4dd
stergina <45051285+Stergina@users.noreply.github.com>
2020-07-16 01:01:05 +0300

    Update WordFreq.java

--- Binary Search Trees/WordFreq.java
+++ Binary Search Trees/WordFreq.java
@@ -1,27 +1,22 @@
-public class WordFreq 
-{
-	private String word;
-	private int freq;
-	
-	WordFreq(String word)
-	{
-		this.word = word;
-		freq = 0;
-	}
+public class WordFreq {
+    private String word;
+    private int freq;
 
-	WordFreq(String word, int freq)
-	{
-		this.word = word;
-		this.freq = freq;
-	}
-	
-	public String key()
-	{
-		return word;
-	}
-	
-	public String toString()
-	{
-		return key() + "" + freq;
-	}
+    WordFreq(String word) {
+        this.word = word;
+        freq = 0;
+    }
+
+    WordFreq(String word, int freq) {
+        this.word = word;
+        this.freq = freq;
+    }
+
+    public String key() {
+        return word;
+    }
+
+    public String toString() {
+        return key() + "" + freq;
+    }
 }
