fbd2ea0dc24ecac7e090871fdc088e99fa606cfe
stergina <45051285+Stergina@users.noreply.github.com>
2020-07-16 01:12:12 +0300

    Update IntQueue.java

--- Queues - Implementations of ADT and applications/IntQueue.java
+++ Queues - Implementations of ADT and applications/IntQueue.java
@@ -1,48 +1,47 @@
 import java.io.PrintStream;
-import java.util.NoSuchElementException; 
+import java.util.NoSuchElementException;
 
 /**
  * Defines the methods for a FIFO queue that handles any data type
  */
-public interface IntQueue<T> 
-{
-	
-	/**
-	 * @return true if the queue is empty
-	 */
-	public boolean isEmpty();
+public interface IntQueue < T > {
 
-	/**
-	 * insert an item to the queue
-	 */
-	public void put(T item);
+    /**
+     * @return true if the queue is empty
+     */
+    public boolean isEmpty();
 
-	/**
- 	 * remove and return the oldest item of the queue
- 	 * @return oldest item of the queue
-	 * @throws NoSuchElementException if the queue is empty
-	 */
-	public T get() throws NoSuchElementException;
+    /**
+     * insert an item to the queue
+     */
+    public void put(T item);
 
-	/**
-	 * return without removing the oldest item of the queue
- 	 * @return oldest item of the queue
-	 * @throws NoSuchElementException if the queue is empty
-	 */
-	public T peek() throws NoSuchElementException;
+    /**
+     * remove and return the oldest item of the queue
+     * @return oldest item of the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public T get() throws NoSuchElementException;
 
-	/**
-	 * print the elements of the queue, starting from the oldest 
+    /**
+     * return without removing the oldest item of the queue
+     * @return oldest item of the queue
+     * @throws NoSuchElementException if the queue is empty
+     */
+    public T peek() throws NoSuchElementException;
+
+    /**
+     * print the elements of the queue, starting from the oldest 
      * item, to the print stream given as argument. For example, to 
      * print the elements to the
-	 * standard output, pass System.out as parameter. E.g.,
-	 * printQueue(System.out);
-	 */
-	public void printQueue(PrintStream stream);
+     * standard output, pass System.out as parameter. E.g.,
+     * printQueue(System.out);
+     */
+    public void printQueue(PrintStream stream);
 
-	/**
-	 * return the size of the queue, 0 if it is empty
-	 * @return number of elements in the queue
-	 */
-	public int size();
+    /**
+     * return the size of the queue, 0 if it is empty
+     * @return number of elements in the queue
+     */
+    public int size();
 }
