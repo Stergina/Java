d569e8a3af0a44576665b1df44405281560682af
stergina <45051285+Stergina@users.noreply.github.com>
2020-07-16 01:14:42 +0300

    Update DefaultComparator.java

--- Sorting and Priority Queues/DefaultComparator.java
+++ Sorting and Priority Queues/DefaultComparator.java
@@ -1,9 +1,7 @@
 import java.util.Comparator;
 
-final class DefaultComparator<T extends Comparable<T>> implements Comparator<T> 
-{
-	public int compare(T a, T b) 
-	{
-		return a.compareTo(b);
-	}
+final class DefaultComparator < T extends Comparable < T >> implements Comparator < T > {
+    public int compare(T a, T b) {
+        return a.compareTo(b);
+    }
 }
