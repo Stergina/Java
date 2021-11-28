991c3486a82610bce52b0fa6895ed1da1b621237
stergina <45051285+Stergina@users.noreply.github.com>
2021-11-28 23:06:05 +0200

    Add files via upload

---
+++ Song Recommendation System/src/DefaultComparator.java
@@ -0,0 +1,9 @@
+import java.util.Comparator;
+
+final class DefaultComparator<T extends Comparable<T>> implements Comparator<T> 
+{
+	public int compare(T a, T b) 
+	{
+		return a.compareTo(b);
+	}
+}
